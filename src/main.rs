70f0038c30c285ea85e716baa83c9316fe996963
vrn21 <killmongervrn@gmail.com>
2023-06-22 16:02:27 +0000

    SOme changes

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ fn main() {
     //assigning variable
     //let a = 5;
     //let mut b = 6;
+    //true is true
+    //false is false
 
     //println!("") ! this shows that this is a macro
     //println!("{a}"); var a wil be print
     //println!("hy adsfgh {:?}",a);
+    //prinln!("{} {} {} {} {} {}",a,b,c,d,e);
 
     //defining a fn
     //fn add(a : i32, b : i32) ->i32 {fn statements}
@@ -23,40 +26,9 @@ fn main() {
     // let mut name = "Varun";
     // println!("My first name is {name}");
     // name = "KV";
-    fn fname() {
-        println!("Varun");
-    }
-    fn lname() {
-        print!("KV");
-    }
-    fname();
-    lname();
-    println!("hi rust running : )");
-    let a = 5;
-    let b = 6;
-    println!(
-        "{:?} {:?} {:?} {:?}",
-        { add(a, b) },
-        { sub(a, b) },
-        { mult(a, b) },
-        { div(a, b) }
-    )
 }
 
 //cargo run --bin <filenamne>
 //this will run only that file
 //for not displaying all the compiling data use -q attribute
-//cargo run -q --bin main
-
-fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
-fn sub(a: i32, b: i32) -> i32 {
-    a - b
-}
-fn mult(a: i32, b: i32) -> i32 {
-    a * b
-}
-fn div(a: i32, b: i32) -> i32 {
-    a / b
-}
+//cargo run -q --bin main <without ext>
