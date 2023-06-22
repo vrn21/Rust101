96436f4bf662492c7cca3c10ff2efe36a72695d5
vrn21 <116856685+vrn21@users.noreply.github.com>
2023-06-22 13:01:46 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    //assigning variable
+    //let a = 5;
+    //let mut b = 6;
+
+    //println!("") ! this shows that this is a macro
+    //println!("{a}"); var a wil be print
+    //println!("hy adsfgh {:?}",a);
+
+    //defining a fn
+    //fn add(a : i32, b : i32) ->i32 {fn statements}
+    //here i32 shows integer of 32 bit size. -> i32 shows that the output is of that datatype
+
+    //loop while loop is also there
+    /*let mut i = 0;
+    loop {
+        println!("the number is {i}");
+        if i == 5 {
+            break;
+        }
+        i = i + 1;
+    }*/
+    // let mut name = "Varun";
+    // println!("My first name is {name}");
+    // name = "KV";
+     println!("hi rust running ");
+}
---
+++ target/debug/my-project.d
@@ -0,0 +1,1 @@
+/home/runner/rust101/target/debug/my-project: /home/runner/rust101/src/main.rs
