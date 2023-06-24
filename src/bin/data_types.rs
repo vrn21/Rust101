87981d253e7f31cd654d7a521832fc25d0a134fa
vrn21 <116856685+vrn21@users.noreply.github.com>
2023-06-24 07:23:16 +0000

    Enum added

---
+++ src/bin/data_types.rs
@@ -0,0 +1,23 @@
+//enum
+    enum Direction {
+        Up,
+        Down,
+        Left,
+        Right, //comma on last value is optional
+    }
+//here the up down .. are called variants in an enum
+    //lets say there is a fn that  is acceseing these values from enum
+
+fn main() {
+   let Go = Direction::Left;
+   match Go{
+     Direction::Left => println!("Go left"),
+     Direction::Up => println!("Go U"),
+     Direction::Down => println!("Go D"),
+     Direction::Right => println!("Go Right"),
+     
+   } 
+
+    
+    
+}
